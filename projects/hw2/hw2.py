5872c28c9b265f0c4fe69789c5a770c798f5adfa
Kyle Remmenga <kyleremmenga@gmail.com>
2025-09-13 15:29:36 -0600

    Fixed hw2 downsampling error

--- projects/hw2/hw2.py
+++ projects/hw2/hw2.py
@@ -1,4 +1,4 @@
-from skimage import io, img_as_float32
+from skimage import io, img_as_float32, transform
 from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,28 +7,36 @@ def main():
     img = io.imread("headshot.jpg")
     float_img = img_as_float32(img[:, :, :3])
     gray_img = rgb2gray(float_img)
-    f1 = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
-    f2_1 = np.array([[0, 0, 0], [0, 2, 0], [0, 0, 0]])
-    f2_2 = (1/9) * np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
-    f3 = (1/16) * np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]])
+    kernal1 = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+    kernal2 = np.array([[0, 0, 0], [0, 2, 0], [0, 0, 0]]) - ((1/9) * np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]]))
+    kernal3 = (1/16) * np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]])
     
     
-    downsampled_img = gray_img[119:221, 74:176]
-    f1_img = np.empty((100, 100), dtype=np.float32)
-    f2_img = np.empty((100, 100), dtype=np.float32)
-    f3_img = np.empty((100, 100), dtype=np.float32)
-    for y in range(100):
-        for x in range(100):
-            kernal = np.array([[downsampled_img[y, x], downsampled_img[y, x+1], downsampled_img[y, x+2]], 
+    downsampled_img = transform.rescale(gray_img, 0.5)
+    height, width = downsampled_img.shape
+    f1_img = np.empty([height - 2, width - 2])
+    f2_img = np.empty([height - 2, width - 2])
+    f3_img = np.empty([height - 2, width - 2])
+    
+    for y in range(height - 2):
+        for x in range(width - 2):
+            img_sample = np.array([[downsampled_img[y, x], downsampled_img[y, x+1], downsampled_img[y, x+2]], 
                                [downsampled_img[y+1, x], downsampled_img[y+1, x+1], downsampled_img[y+1, x+2]],
                                [downsampled_img[y+2, x], downsampled_img[y+2, x+1], downsampled_img[y+2, x+2]]])
-            # f1
-            f1_img[y, x] = np.sum(kernal * f1)
-            # f2
-            f2_img[y, x] = np.sum(kernal * f2_1) - np.sum(kernal * f2_2)
-            # f3
-            f3_img[y, x] = np.sum(kernal * f3)
             
+            # f1
+            new_value = np.sum(img_sample * kernal1)
+            if new_value < 0:
+                f1_img[y, x] = 0
+            elif new_value > 1:
+                f1_img[y, x] = 1
+            else:
+                f1_img[y, x] = new_value
+            # f2
+            f2_img[y, x] = np.sum(img_sample * kernal2)
+            # f3
+            f3_img[y, x] = np.sum(img_sample * kernal3)
+       
     plt.imsave("deliverables/downsampled_img.png", downsampled_img, cmap="gray")
     plt.imsave("deliverables/f1_img.png", f1_img, cmap="gray")
     plt.imsave("deliverables/f2_img.png", f2_img, cmap="gray")
